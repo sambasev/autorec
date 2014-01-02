c6d8059d66f85fe9808ab8f7e9f3104d340d543b
sambasevam <sambasevam@yahoo.co.in>
2014-01-02 09:45:44 -0800

    need to implement these guys

--- Autorec/autorec.cpp
+++ Autorec/autorec.cpp
@@ -8,10 +8,59 @@ autorec::autorec(audioMasterCallback audioMaster) :
 AudioEffectX(audioMaster, 0, NUM_PARAMS) {
 
 }
+
 autorec::~autorec() {
 
 }
 
 void autorec::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) {
 	//Actual processing
+}
+
+void autorec::processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames){
+
+}
+
+void autorec::setProgramName(char* name){
+
+}
+
+void autorec::getProgramName(char* name){
+
+}
+
+void autorec::setParameter(VstInt32 index, float value){
+
+}
+
+float autorec::getParameter(VstInt32 index){
+	return 0;
+}
+
+void autorec::getParameterLabel(VstInt32 index, char* label){
+
+}
+
+void autorec::getParameterDisplay(VstInt32 index, char* text){
+
+}
+
+void autorec::getParameterName(VstInt32 index, char* text){
+
+}
+
+bool autorec::getEffectName(char* name){
+	return true;
+}
+
+bool autorec::getVendorString(char* text){
+	return true;
+}
+
+bool autorec::getProductString(char* text){
+	return true;
+}
+
+VstInt32 autorec::getVendorVersion(){
+	return 0;
 }
--- Autorec/autorec.h
+++ Autorec/autorec.h
@@ -1,4 +1,5 @@
 #include "audioeffectx.h"
+
 #define NUM_PARAMS 0
 
 class autorec : public AudioEffectX {
@@ -7,4 +8,22 @@ public:
 	~autorec();
 
 	void processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames);
+	virtual void processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames);
+	
+	// Program
+	virtual void setProgramName(char* name);
+	virtual void getProgramName(char* name);
+
+	// Parameters
+	virtual void setParameter(VstInt32 index, float value);
+	virtual float getParameter(VstInt32 index);
+	virtual void getParameterLabel(VstInt32 index, char* label);
+	virtual void getParameterDisplay(VstInt32 index, char* text);
+	virtual void getParameterName(VstInt32 index, char* text);
+
+	virtual bool getEffectName(char* name);
+	virtual bool getVendorString(char* text);
+	virtual bool getProductString(char* text);
+	virtual VstInt32 getVendorVersion();
+
 };
